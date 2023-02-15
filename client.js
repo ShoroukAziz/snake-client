2bbd2221a55b3d817445151a9099921f6ec4fcf3
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-02-15 16:24:48 -0700

    send your name to the server

---
+++ client.js
@@ -0,0 +1,32 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: 50541
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("connect",()=> {
+    console.log('Successfully connected to game server');
+    conn.write("Name: SHA");
+  });
+
+  conn.on("data",(incomingMessage)=>{
+    console.log(`Server says: ${incomingMessage}`);
+  })
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+module.exports = connect;
+
+module.exports = {
+  connect
+};
